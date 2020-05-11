39878b21ac3c1ec6435a2ab1d78ac0967ce801cd
Carl <diadophus@gmail.com>
2020-05-10 23:08:58 -0600

    simplex based value and perlin noise

---
+++ graphics/noise/simplex_value.glsl
@@ -0,0 +1,177 @@
+
+
+float get_2d_simplex_perlin_noise(
+    in vec2 V,
+    in vec2 K 
+){
+    const float s = (sqrt(3.0)-1.0)/2.0;
+    const mat2 S = mat2(1.0+s, s,
+                        s, 1.0+s);
+    const mat2 Sinv = inverse(S);
+    vec2 F  = fract(S*V);
+    // simplex vertex indices
+    vec2 I0 = floor(S*V); 
+    vec2 I1 = I0 + (F.x > F.y? vec2(1,0) : vec2(0,1));
+    vec2 I2 = I0 + vec2(1,1);
+    // simplex vertex positions
+    vec2 V0 = Sinv*I0;
+    vec2 V1 = Sinv*I1;
+    vec2 V2 = Sinv*I2;
+    // simplex vertex position offsets
+    vec2 F0 = V-V0;
+    vec2 F1 = V-V1;
+    vec2 F2 = V-V2;
+    float t0 = max( 0.5 - dot(F0, F0), 0.0); t0*=t0; t0*=t0; 
+    float t1 = max( 0.5 - dot(F1, F1), 0.0); t1*=t1; t1*=t1; 
+    float t2 = max( 0.5 - dot(F2, F2), 0.0); t2*=t2; t2*=t2; 
+    return 30.0 * (
+        t0 * noise2(dot(I0,K)) +
+        t1 * noise2(dot(I1,K)) +
+        t2 * noise2(dot(I2,K))
+    );
+}
+
+/*
+ * Efficient simplex indexing functions by Bill Licea-Kane, ATI. Thanks!
+ */
+void get_3d_simplex_vertex_index_offsets( 
+    const in vec3 P, 
+    out vec3 offset1, 
+    out vec3 offset2 
+){
+    vec3 offset0;
+
+    vec2 isX = step( P.yz, P.xx ); // P.x >= P.y ? 1.0 : 0.0;  P.x >= P.z ? 1.0 : 0.0;
+    offset0.x  = isX.x + isX.y;    // Accumulate all P.x >= other channels in offset.x
+    offset0.yz = 1.0 - isX;        // Accumulate all P.x <  other channels in offset.yz
+
+    float isY = step( P.z, P.y );  // P.y >= P.z ? 1.0 : 0.0;
+    offset0.y += isY;              // Accumulate P.y >= P.z in offset.y
+    offset0.z += 1.0 - isY;        // Accumulate P.y <  P.z in offset.z
+
+    // offset0 now contains the unique values 0,1,2 in each channel
+    // 2 for the channel greater than other channels
+    // 1 for the channel that is less than one but greater than another
+    // 0 for the channel less than other channels
+    // Equality ties are broken in favor of first x, then y
+    // (z always loses ties)
+
+    offset2 = clamp( offset0, 0.0, 1.0 );
+    // offset2 contains 1 in each channel that was 1 or 2
+    offset1 = clamp( offset0-1.0, 0.0, 1.0 );
+    // offset1 contains 1 in the single channel that was 1
+}
+
+float get_3d_simplex_perlin_noise(
+    in vec3 V,
+    in vec3 K 
+){
+    const float s = 1.0/3.0;
+    const mat3 S = mat3(1.0+s, s, s,
+                        s, 1.0+s, s,
+                        s, s, 1.0+s);
+    const mat3 Sinv = inverse(S);
+    vec3 F  = fract(S*V);
+    // simplex vertex index offsets
+    vec3 O1; vec3 O2; get_3d_simplex_vertex_index_offsets(F, O1, O2);
+    // simplex vertex indices
+    vec3 I0 = floor(S*V); 
+    vec3 I1 = I0 + O1;
+    vec3 I2 = I0 + O2;
+    vec3 I3 = I0 + vec3(1,1,1);
+    // simplex vertex positions
+    vec3 V0 = Sinv*I0;
+    vec3 V1 = Sinv*I1;
+    vec3 V2 = Sinv*I2;
+    vec3 V3 = Sinv*I3;
+    // simplex vertex position offsets
+    vec3 F0 = V-V0;
+    vec3 F1 = V-V1;
+    vec3 F2 = V-V2;
+    vec3 F3 = V-V3;
+    float t0 = max( 0.5 - dot(F0, F0), 0.0); t0*=t0; t0*=t0; 
+    float t1 = max( 0.5 - dot(F1, F1), 0.0); t1*=t1; t1*=t1; 
+    float t2 = max( 0.5 - dot(F2, F2), 0.0); t2*=t2; t2*=t2; 
+    float t3 = max( 0.5 - dot(F3, F3), 0.0); t3*=t3; t3*=t3; 
+    return 32.0 * (
+        t0 * noise3(dot(I0,K)) +
+        t1 * noise3(dot(I1,K)) +
+        t2 * noise3(dot(I2,K)) +
+        t3 * noise3(dot(I3,K))
+    );
+}
+
+/*
+ * Efficient simplex indexing functions by Bill Licea-Kane, ATI. Thanks!
+ */
+void get_4d_simplex_vertex_index_offsets( 
+    const in vec4 P, 
+    out vec4 offset1, 
+    out vec4 offset2, 
+    out vec4 offset3 
+){
+    vec4 offset0;
+
+    vec3 isX = step( P.yzw, P.xxx );        // See comments in 3D simplex function
+    offset0.x = isX.x + isX.y + isX.z;
+    offset0.yzw = 1.0 - isX;
+
+    vec2 isY = step( P.zw, P.yy );
+    offset0.y += isY.x + isY.y;
+    offset0.zw += 1.0 - isY;
+
+    float isZ = step( P.w, P.z );
+    offset0.z += isZ;
+    offset0.w += 1.0 - isZ;
+
+    // offset0 now contains the unique values 0,1,2,3 in each channel
+
+    offset3 = clamp( offset0, 0.0, 1.0 );
+    offset2 = clamp( offset0-1.0, 0.0, 1.0 );
+    offset1 = clamp( offset0-2.0, 0.0, 1.0 );
+}
+
+float get_4d_simplex_perlin_noise(
+    in vec4 V,
+    in vec4 K 
+){
+    const float s =  (sqrt(5.0)-1.0)/4.0;
+    const mat4 S = mat4(1.0+s, s, s, s,
+                        s, 1.0+s, s, s,
+                        s, s, 1.0+s, s,
+                        s, s, s, 1.0+s);
+    const mat4 Sinv = inverse(S);
+    vec4 F  = fract(S*V);
+    // simplex vertex index offsets
+    vec4 O1; vec4 O2; vec4 O3; get_4d_simplex_vertex_index_offsets(F, O1, O2, O3);
+    // simplex vertex indices
+    vec4 I0 = floor(S*V); 
+    vec4 I1 = I0 + O1;
+    vec4 I2 = I0 + O2;
+    vec4 I3 = I0 + O3;
+    vec4 I4 = I0 + vec4(1,1,1,1);
+    // simplex vertex positions
+    vec4 V0 = Sinv*I0;
+    vec4 V1 = Sinv*I1;
+    vec4 V2 = Sinv*I2;
+    vec4 V3 = Sinv*I3;
+    vec4 V4 = Sinv*I4;
+    // simplex vertex position offsets
+    vec4 F0 = V-V0;
+    vec4 F1 = V-V1;
+    vec4 F2 = V-V2;
+    vec4 F3 = V-V3;
+    vec4 F4 = V-V4;
+    float t0 = max( 0.5 - dot(F0, F0), 0.0); t0*=t0; t0*=t0; 
+    float t1 = max( 0.5 - dot(F1, F1), 0.0); t1*=t1; t1*=t1; 
+    float t2 = max( 0.5 - dot(F2, F2), 0.0); t2*=t2; t2*=t2; 
+    float t3 = max( 0.5 - dot(F3, F3), 0.0); t3*=t3; t3*=t3; 
+    float t4 = max( 0.5 - dot(F4, F4), 0.0); t4*=t4; t4*=t4; 
+    return 27.0 * (
+        t0 * noise4(dot(I0,K)) +
+        t1 * noise4(dot(I1,K)) +
+        t2 * noise4(dot(I2,K)) +
+        t3 * noise4(dot(I3,K)) +
+        t4 * noise4(dot(I4,K))
+    );
+}
--- graphics/noise/value.glsl
+++ graphics/noise/value.glsl
@@ -8,18 +8,19 @@ float get_2d_value_noise(
     in vec2 K
 ){
     vec2 I = floor(V);
-    vec2 F = fract(V);
-    vec2 G = smoothstep(0.f, 1.f, F);
-    vec2 J = vec2(0.f);
+    vec2 F = smoothstep(0.f, 1.f, fract(V));
+    vec2 G = vec2(0);
+    vec2 O = vec2(0);
+    vec2 J = vec2(0);
     float a = 0.f;
-    for (int i = 0; i < 1; ++i)
+    for (int i = 0; i <= 1; ++i)
     {
-        for (int j = 0; j < 1; ++j)
+        for (int j = 0; j <= 1; ++j)
         {
-            J = I + vec2(i,j);
-            a += noise1(dot(J,K))
-                * (i==0? 1.f-G.x : G.x)
-                * (j==0? 1.f-G.y : G.y);
+            O = vec2(i,j);
+            J = I + O;
+            G = mix(1.0f-F, F, O);
+            a += noise1(dot(J,K)) * G.x * G.y;
         }
     }
     return clamp(a, 0.f, 1.f);
@@ -33,8 +34,10 @@ float get_3d_value_noise(
     in vec3 K 
 ){
     vec3 I = floor(V);
-    vec3 F = fract(V);
-    vec3 G = smoothstep(0.f, 1.f, F);
+    vec3 F = smoothstep(0.f, 1.f, fract(V));
+    vec3 G = vec3(0);
+    vec3 O = vec3(0);
+    vec3 J = vec3(0);
     float a = 0.f;
     for (int i = 0; i <= 1; ++i)
     {
@@ -42,10 +45,10 @@ float get_3d_value_noise(
         {
             for (int k = 0; k <= 1; ++k)
             {
-                a += noise1(dot(I+vec3(i,j,k), K)) 
-                    * (i==0? 1.f-G.x : G.x) 
-                    * (j==0? 1.f-G.y : G.y) 
-                    * (k==0? 1.f-G.z : G.z); 
+                O = vec3(i,j,k);
+                J = I + O;
+                G = mix(1.0f-F, F, O);
+                a += noise1(dot(J,K)) * G.x * G.y * G.z;
             }
         }
     }
@@ -57,12 +60,14 @@ V: position
 K: seed
 */
 float get_4d_value_noise(
-    in vec3 V,
-    in vec3 K 
+    in vec4 V,
+    in vec4 K 
 ){
-    vec3 I = floor(V);
-    vec3 F = fract(V);
-    vec3 G = smoothstep(0.f, 1.f, F);
+    vec4 I = floor(V);
+    vec4 F = smoothstep(0.f, 1.f, fract(V));
+    vec4 G = vec4(0);
+    vec4 O = vec4(0);
+    vec4 J = vec4(0);
     float a = 0.f;
     for (int i = 0; i <= 1; ++i)
     {
@@ -72,11 +77,10 @@ float get_4d_value_noise(
             {
                 for (int l = 0; l <= 1; ++l)
                 {
-                    a += noise1(dot(I+vec3(i,j,k,l), K)) 
-                        * (i==0? 1.f-G.x : G.x) 
-                        * (j==0? 1.f-G.y : G.y) 
-                        * (k==0? 1.f-G.z : G.z) 
-                        * (l==0? 1.f-G.w : G.w);
+                    O = vec4(i,j,k,l);
+                    J = I + O;
+                    G = mix(1.0f-F, F, O);
+                    a += noise1(dot(J,K)) * G.x * G.y * G.z * G.w;
                 }
             }
         }
